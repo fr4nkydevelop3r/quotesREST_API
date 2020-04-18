0d25d07f2b4dca8f2b1269262a40bddf71eecac5
Frank Andrade <fr4nky.develop3r@gmail.com>
2020-04-17 22:46:44 -0400

    testing

--- dist/index.js
+++ dist/index.js
@@ -14,7 +14,7 @@ var server = _http["default"].createServer(function (req, res) {
     'Content-Type': 'text/plain'
   });
   res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+}).listen(process.env.PORT, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
 var _default = server;
